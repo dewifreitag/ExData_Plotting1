a5ddfd2e0654d5f3b9889057e96328ad47fa4b3f
dewifreitag <jasinta_dewi@yahoo.co.uk>
2015-01-09 07:53:43 +0100

    plot4.R corrected

--- plot4.R
+++ plot4.R
@@ -25,7 +25,8 @@ lines(feb.data$DateTime, feb.data$Sub_metering_2, type="l", lwd=2,
 lines(feb.data$DateTime, feb.data$Sub_metering_3, type="l", lwd=2, 
             col="blue")
 legend("topright", lty=1, col=c("black", "red", "blue"), 
-       legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+       legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
+       box.col = "transparent")
 
 # bottomright
 plot(feb.data$DateTime, feb.data$Global_reactive_power, type="l", lwd=2,
